545348f4047a2e5f8e8f8938bfb659fa0a18c63d
B-Rydalch <bradrydalch@gmail.com>
2018-03-04 02:53:35 -0700

    submitting neural net assignment

--- Prove-Assignments/prove06(Neural Network)/network_layer.py
+++ Prove-Assignments/prove06(Neural Network)/network_layer.py
@@ -1,36 +1,330 @@
-from neuron import Neuron
+import numpy as np
+import random as rn
+import operator
+from math import exp
+import matplotlib.pyplot as plt
 
-#######################################
-# NETWORK LAYER REQUIREMENTS 
-# number neurons
-# number attributes -> inputs/weights
-# threshhold value
-#######################################
+class NeuralNetworkCalssifier:
+    def __init__(self):
+        self.num_nodes = []
+        self.num_hidden_layers = int(input("With how many hidden layers?\n> "))
+        for i in range(self.num_hidden_layers):
+            self.num_nodes.append(int(input("How many nodes in layer " + str(i + 1) + "?\n> ")))
 
-class Network_Layer: 
-    def __init__(self, num_neurons, num_attributes, threshhold):
-        self.num_neurons = num_neurons
-        self.neurons = []
-        self.num_attributes = num_attributes + 1 # add 1 for bias
-        self.threshhold = threshhold
+    def fit(self, data, target):
+        num_rows, num_cols = data.shape        
+        neural_network = Neurons(num_cols, self.num_hidden_layers, self.num_nodes, target)
+        neural_network.teach(data)
+
+        return NeuralNetworkModel(neural_network)
+
+
+# the node which holds the weights between a neuron and the targets
+class Target_Node:
+    def __init__(self, num_inputs, target=None):
+        self.input_weights = []
+        self.target = target
+        self.value = 0
+        self.delta = 0
+        self.bias = -1
+
+        # assign random weight values
+        for _ in range(num_inputs + 1):
+            self.input_weights.append(rn.uniform(-1, 1))
+
+    # sigmoid function to determine whether or not the neuron fires
+    @staticmethod
+    def sigmoid(value):
+        return 1 / (1 + exp(-value))
+
+    # trains to have correct weights
+    def train(self, data_row):
+        self.value = 0
         
-        for i in range(0,self.num_neurons):
-            self.neurons.append(Neuron(self.num_attributes,self.threshhold))
+        # gets the sum of the weights times the data input
+        for index in range(len(data_row)):
+            self.value += data_row[index] * self.input_weights[index + 1]
 
-    def input_row(self, input_):
-        results = []
-        data = list(input_.copy())
-        for i in self.neurons:
-            results.append(i.input(data))
-        return results
+        # add the biased node
+        self.value += self.bias * self.input_weights[0]
+        self.value = self.sigmoid(self.value)
 
-    def input(self, input_matrix):
-        results = [] 
-        for row in input_matrix:
-            results.append(self.input_row(row))
-        return results
+        return self.value
 
-    def __repr__(self):
-        pass
 
+class Neurons:
+    def __init__(self, num_cols, num_hidden_layers, num_nodes, targets):
+        self.neural_network = [[] for _ in range(num_hidden_layers + 1)]
+        self.targets = targets
+        self.unique_targets = set(targets)
+        self.num_hidden_layers = num_hidden_layers
        
+        if num_hidden_layers > 0:
+            # create first layer 
+            for node in range(num_nodes[0]):
+                self.neural_network[0].append(Target_Node(num_cols))
+
+            # create the hidden layers
+            for index in range(num_hidden_layers - 1):
+                for _ in range(num_nodes[index + 1]):
+                    self.neural_network[index + 1].append(Target_Node(len(self.neural_network[index])))
+
+            # create the output layer
+            for unique_target in self.unique_targets:
+                self.neural_network[num_hidden_layers].append(Target_Node(
+                    len(self.neural_network[num_hidden_layers - 1]), unique_target))
+
+        else:  # No hidden layers, only input and output
+            for unique_target in self.unique_targets:
+                self.neural_network[0].append(Target_Node(num_cols, unique_target))
+
+    # teaches the neuron array when to fire when given data
+    def teach(self, data):
+       
+        # this will run when either all the weights are correct or after 1000 runs
+        done = False
+        runs = 0
+        accuracy = []
+        ac = self.get_accuracy(data)
+        accuracy.append(ac)
+        
+        print("Starting accuracy: " + str(round(ac * 100, 3)) + "\n")
+        
+        if self.num_hidden_layers > 0:
+            
+            while not done and runs < 1000:
+                # everything was predicted correctly
+                done = True
+               
+                # teach counter
+                runs += 1
+                
+                # loop through each row of data
+                for index, data_row in enumerate(data):
+                    
+                    # 2D array to keep track of nodes values at each layer
+                    if self.num_hidden_layers > 1:
+                        hidden_node_values = [[] for _ in range(self.num_hidden_layers)]
+                    else:
+                        hidden_node_values = [[]]
+                    
+                    # set up the first layer with the data as inputs
+                    for node in self.neural_network[0]:
+                        hidden_node_values[0].append(node.train(data_row))
+                    
+                    # set up all the hidden ayers with the previous layer's activation as the value
+                    for layer_index, layer in enumerate(self.neural_network[1:-1]):
+                        for node in layer:
+                            hidden_node_values[layer_index + 1].append(node.train(hidden_node_values[layer_index]))
+                   
+                    # a dictionary with the target name as the key and its activation as the value
+                    target_values = dict()
+                    
+                    # gets the activation for each target
+                    for node in self.neural_network[-1]:
+                        target_values[node.target] = node.train(hidden_node_values[-1])
+                    
+                    # get the target with the highest activation value
+                    prediction = min(target_values, key=target_values.get)#self.get_key_with_max_value(target_values)
+                    
+                    # if the highest activation value was the correct target, it predicted correctly!
+                    if self.targets[index] != prediction:
+                        self.recalculate_node_values(prediction, self.targets[index], data_row)
+                        done = False
+                accuracy.append(self.get_accuracy(data) * 100)
+
+        else:  # No hidden layers
+            
+            # runs either 1000 times or if it guesses every target correctly
+            while not done and runs < 1000:
+                # if this never changes, everything was predicted correctly
+                done = True
+                
+                # tests counter
+                runs += 1
+                
+                # loop through each row of data
+                for index, data_row in enumerate(data):
+                    # a dictionary with the target name as the key and its activation as the value
+                    target_values = dict()
+                    
+                    # loop through each node in the neural network and calculate its activation
+                    for node in self.neural_network[0]:
+                        target_values[node.target] = node.train(data_row)
+                    
+                    # get the target with the highest activation value
+                    prediction = min(target_values, key=target_values.get)#self.get_key_with_max_value(target_values)
+                    
+                    # if the highest activation value was the correct target, it predicted correctly!
+                    if self.targets[index] != prediction:
+                        self.recalculate_node_values(prediction, self.targets[index], data_row)
+                        
+                        # if did not guess correctly, we're going to have to loop again.
+                        done = False
+                accuracy.append(self.get_accuracy(data) * 100)
+        print("TEST DONE")
+        print("ENDING ACCURACY: " + str(round(accuracy[-1], 3)))
+        x = range(0, len(accuracy), 1)
+        plt.close()
+        plt.plot(x, accuracy)
+        plt.show()
+
+    # recalculate 
+    def recalculate_node_values(self, wrongly_predicted_target, correct_target, data):
+        self.recalculate_deltas(wrongly_predicted_target, correct_target)
+        
+        if self.num_hidden_layers > 0:
+            
+            # Loop through the target nodes and change the weights for the required targets
+            for node in self.neural_network[-1]:
+                
+                # Only need to change the weights of the targets that should have been
+                # predicted and weren't or that were wrongly predicted
+                if node.target == correct_target or node.target == wrongly_predicted_target:
+                    # Reassign the weights of the node
+                    self.calc_weights(node, self.neural_network, -2)
+            
+            # Now loop through every layer between the output layer and the first hidden layer
+            for layer_index, layer in enumerate(self.neural_network[1:-1]):
+                for node in layer:
+                    # Recalculate the node's weights
+                    self.calc_weights(node, self.neural_network, layer_index)
+
+        # recalculate the weights for the first hidden layer (or only layer if no hidden layers)
+        # the data is the input this time, no previous nodes to get values from
+        for node in self.neural_network[0]:
+            self.calc_weights(node, data)
+
+    # recalculate the weights of a particular node.  layer_index will determine which layer of the neural
+    # network this node resides, unless it is on the first (or only) layer, in which case there will be no layer_index
+    @staticmethod
+    def calc_weights(node, values, prev_layer_index=None):
+        # n used for calculating new weight
+        n = -.1
+        
+        # this is the first layer in the network.
+        if prev_layer_index is None:
+            
+            # loop through all the weights of vertices that are attached to input values
+            for weight_index in range(len(node.input_weights) - 1):
+                # reassign the node's weight
+                node.input_weights[weight_index + 1] = node.input_weights[weight_index + 1] - (
+                        n * node.delta * values[weight_index])
+           
+            # calculate the new weight for the bias node
+            node.input_weights[0] = node.input_weights[0] - (n * node.delta * node.bias)
+        
+        else:  # this node resides in a hidden layer that isn't the first layer
+           
+            # loop through all the weights of vertices that are attached to input values
+            for weight_index in range(len(node.input_weights) - 1):
+                # reassign the node's weight
+                node.input_weights[weight_index + 1] = node.input_weights[weight_index + 1] - (
+                        n * node.delta * values[prev_layer_index][weight_index].value)
+            
+            # Calculate the new weight for the bias node
+            node.input_weights[0] = node.input_weights[0] - (n * node.delta * node.bias)
+
+    # recalculates the required deltas for the nodes
+    def recalculate_deltas(self, wrongly_predicted_target, correct_target):
+        
+        # loop through each target node and recalculate if needed
+        for target_node in self.neural_network[-1]:
+            if target_node.target == correct_target:
+               
+                # calculate the error of the target node
+                target_node.delta = target_node.value * (1 - target_node.value) * (target_node.value - 1)
+            
+            elif target_node.target == wrongly_predicted_target: 
+                
+                # calculate the error
+                target_node.delta = target_node.value * (1 - target_node.value) * target_node.value
+        
+        # if there are more hidden layers, we're not done yet
+        if self.num_hidden_layers > 0:
+            
+            # start at the hidden layers closest to the target and work backwards
+            hidden_layer_index = len(self.neural_network) - 2
+            
+            # go through all the hidden layers 
+            while hidden_layer_index >= 0:
+                
+                # loop through each node in this hidden layer
+                for node_index, node in enumerate(self.neural_network[hidden_layer_index]):
+                   
+                    # calculate the sum for each next layer node's delta multiplied by the
+                    # weight between that node and this node
+                    sum_delta_weights = 0
+                    for prev_node in self.neural_network[hidden_layer_index + 1]:
+                        sum_delta_weights += prev_node.input_weights[node_index + 1] * prev_node.delta
+                   
+                    # calculate the new delta for the node
+                    node.delta = node.value * (1 - node.value) * sum_delta_weights
+                
+                # decrement the index
+                hidden_layer_index -= 1
+
+    # gets the key of the item with the max value in a dictionary
+    @staticmethod
+    def get_key_with_max_value(dictionary):
+        return max(dictionary.items(), key=operator.itemgetter(1))[0]
+
+    # gets the accuracy of the current iteration
+    def get_accuracy(self, data):
+        num_predicted_correctly = 0
+        for index, data_row in enumerate(data):
+            if self.predict(data_row) == self.targets[index]:
+                num_predicted_correctly += 1
+
+        return num_predicted_correctly / len(self.targets)
+
+    # predicts the target for a particular row of data
+    def predict(self, data_row):
+        
+        if self.num_hidden_layers > 0:
+            
+            # 2D array to keep track of nodes values at each layer
+            if self.num_hidden_layers > 1:
+                hidden_node_values = [[] for _ in range(self.num_hidden_layers)]
+            else:
+                hidden_node_values = [[]]
+           
+            # set up the first layer as inputs
+            for node in self.neural_network[0]:
+                hidden_node_values[0].append(node.train(data_row))
+            
+            # set up all the hidden layers with the previous layer's activation as the value
+            for layer_index, layer in enumerate(self.neural_network[1:-1]):
+                for node in layer:
+                    hidden_node_values[layer_index + 1].append(node.train(hidden_node_values[layer_index]))
+            
+            target_values = dict()
+            
+            # gets the activation for each target
+            for node in self.neural_network[-1]:
+                target_values[node.target] = node.train(hidden_node_values[-1])
+            
+            # Predicts the target with the highest activation value
+            return min(target_values, key=target_values.get)#self.get_key_with_max_value(target_values)
+        
+        else:  # No hidden layers
+            target_values = dict()
+            
+            # gets the activation for each target
+            for node in self.neural_network[0]:
+                target_values[node.target] = node.train(data_row)
+            
+            # predicts the target with the highest activation value
+            return min(target_values, key=target_values.get)#self.get_key_with_max_value(target_values)
+
+
+class NeuralNetworkModel:
+    def __init__(self, neural_network):
+        self.neural_network = neural_network
+        self.model = []
+
+    def predict(self, data):
+        for data_row in data:
+            self.model.append(self.neural_network.predict(data_row))
+
+        return self.model
--- Prove-Assignments/prove06(Neural Network)/neural_net.py
+++ Prove-Assignments/prove06(Neural Network)/neural_net.py
@@ -1,57 +1,265 @@
+from sklearn import datasets
 import pandas as pd
 import numpy as np
-from neuron import Neuron
-from network_layer import Network_Layer
-from sklearn import datasets
+from scipy.stats import zscore
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import KFold
+from sklearn.neural_network import MLPClassifier
+from network_layer import NeuralNetworkCalssifier
 
 
-def main(): 
+# prompts the user for their desired data set 
+def get_data_set():
+    print("CALLED get_data_set \n")
+    while True:
+        print("What dataset would you like to work with?")
+        print("1 - Iris data set")
+        print("2 - Pima Indian Diabetes data set")
+
+
+        user_input = int(input("> "))
+
+        if user_input == 1:
+            data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
+            columns = ["sepal length", "sepal width", "petal length", "petal width", "class"]
+            data.columns = columns
+            for col in columns:
+                data[col] = data[col].astype("category")
+
+            return data, "Iris", False
+
+        elif user_input == 2:
+            data = pd.read_csv(
+                "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/"
+                "pima-indians-diabetes.data")
+            data.columns = ["pregnancies", "glucose", "blood pressure", "tricep thickness", "insulin", "bmi",
+                            "pedigree", "age", "diabetic"]
+            data["diabetic"].replace([0, 1], ["non-diabetic", "diabetic"], inplace=True)
+            data["diabetic"] = data["diabetic"].astype("category")
+            data.replace(0, np.NaN, inplace=True)
+            data.dropna(inplace=True)
+            return data, "Pima Indian Diabetes", False
+
+        else:
+            print("Not a valid input.")
+
+# gets how many times to test
+def get_num_tests():
+    print("CALLED get_num_tests \n")
     
-    print("Select which dataset to import. \n \
-    1. Iris\n \
-    2. Pima Indian Diabetes")
+    is_number = False
+    k = 3
+    
+    while not is_number or k <= 2:
+        print("How many tests do you want to run?")
+        if k <= 2:
+            print("Must be more than 2 tests.")
 
-    user_input1 = int(input("> "))
+        is_number = True
+        
+        # handles non-integer values
+        try:
+            k = int(input("> "))
+        except:
+            print("You must enter a number!")
+            is_number = False
 
-    print("Enter thresshold value ")
-    user_input2 = (input("> "))
+    return k
+
+
+#  gets the classifier the user wants 
+#  to test with. 
+def get_classifier(is_regressor_data):
+    print("Called get_classifier \n")
+    
+    # prompt user
+    print("Which algorithm would you like to use?")
+    
+    while True:
+        print("1 - scikit-learn Gaussian")
+        print("2 - Hard Coded Neural Network Classifier")
+        print("3 - scikit-learn Neural Network Classifier")
+
+        chosen_algorithm = int(input("> "))
+
+        if chosen_algorithm == 1:
+            return GaussianNB(), "Gaussian"
+        elif chosen_algorithm == 2:
+            return NeuralNetworkCalssifier(), "Hard Coded Neural Network Classifier"
+        elif chosen_algorithm == 3:
+            return MLPClassifier(), "scikit-learn Neural Network Classifier"
+        else:
+            print("Not a valid user_input.")
+
+# gets multiple classifiers in case the user wants 
+# to compare the data against various tests.
+def get_multiple_classifiers(is_regressor_data):
+    print("Called get_mult_classifier")
+    
+    # prompt user for which algorithm
+    classifiers = dict()
+    print("Which algorithms would you like to test?")
+    print("(Enter an algorithm one at a time, pressing enter after each addition)")
+    user_input = ""
+
+    while user_input != "done" and user_input != "Done":
+        print("1 - scikit-learn Gaussian")
+        print("2 - Hard Coded Nerual Network Classifier")
+        print("3 - scikit-learn Neural Network Classifier")
+        print("Type \"done\" when completed.")
+        
+        user_input = int(input("> "))
+        
+        if user_input == 1:
+            classifiers["scikit-learn Gaussian"] = GaussianNB()
+        elif user_input == 2:
+            classifiers["Hard Coded Neural Network Classifier"] = NeuralNetworkCalssifier()
+        elif user_input == 3:
+            classifiers["scikit-learn Neural Network Classifier"] = MLPClassifier()
+        elif user_input != "Done" or user_input != "done":
+            print("Not a valid user_input.")
+
+    while user_input != "done" or user_input != "Done":
+        print("1 - scikit-learn Gaussian")
+        print("2 - Hard Coded Nerual Network Classifier")
+        print("3 - scikit-learn Neural Network Classifier")
+        print("Type \"done\" when completed.")
+        
+        user_input = int(input("> "))
+        
+        if user_input == 1:
+            classifiers["scikit-learn Gaussian"] = GaussianNB()
+        elif user_input == 2:
+            classifiers["Hard Coded Neural Network Classifier"] = NeuralNetworkCalssifier()
+        elif user_input == 3:
+            classifiers["scikit-learn Neural Network Classifier"] = MLPClassifier()
+        elif user_input != "Done" or user_input != "done":
+            print("Not a valid user_input.")
+
+    return classifiers
+
+# cleans/categorizes data
+def clean(data):
+    print("CALLED clean")
    
-    threshhold = user_input2
+    # categorizes the columns that are nominal
+    non_numeric_cols = data.select_dtypes(["category"]).columns
 
+    # replaces all nominal data with numerical values
+    data[non_numeric_cols] = data[non_numeric_cols].apply(lambda x: x.cat.codes)
+
+    # sets the values to their z-score. 
+    # this allows the values have the same weight
+    return data.apply(zscore)
+
+# tests the algorithm k times using k-cross validation
+def k_cross_validation(data_set, algorithm, k, needs_cleaned):
+    print("CALLED k_cross_validation \n")
     
+    kf = KFold(n_splits=k)
+    sum = 0
 
-    if (user_input1 == 1):
-        Iris = datasets.load_iris()
-        neural_net = Network_Layer(3,4,0)
+    # randomize the data
+    data_set = data_set.sample(frac=1)
 
-    elif (user_input1 == 2):
-        train_data, test_data = build_diabetes_dataset()
+    # split data k times and test
+    for train, test in kf.split(data_set):
+        
+        # grab training and testing data
+        train = data_set.iloc[train]
+        test = data_set.iloc[test]
 
-    neuron1 = Neuron(2,threshhold)
+        # grab test accuracy
+        accuracy = test_data(train, test, algorithm, needs_cleaned)
+        sum += accuracy
 
-    print("testing neuron")
-    neuron1.input(2)
-    print (neuron1)
+    # return average accuracy
+    return sum / k
 
+# tests data on a chosen algorithm
+def test_data(train, test, algorithm, needs_cleaned):
+    print("Called test_data \n")
+    
+    # separates the data and puts it into a numpy array
+    if needs_cleaned:
+        train_data = np.array(clean(train[train.columns[0: -1]]))
+        test_data = np.array(clean(test[test.columns[0: -1]]))
+    
+    else:
+        train_data = np.array(train[train.columns[0: -1]])
+        test_data = np.array(test[test.columns[0: -1]])
 
-def build_diabetes_dataset():
+    train_target = np.array(train.iloc[:, -1])
+    test_target = np.array(test.iloc[:, -1])
 
-    headers = ["# preg", "plasma glucose", "Diastolic BP",
-    "Triceps SKT", "serum insulin", "BMI", "Diabetes pedigree"
-    "Age", "Class v"]
+    # train & test the algorithm
+    model = algorithm.fit(train_data, train_target)
+    targets_predicted = model.predict(test_data)
 
-    df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data", header=None, names=headers)
+    # count num of correct predictions
+    count = 0
+    for index in range(len(targets_predicted)):
+        if targets_predicted[index] == test_target[index]:
+            count += 1
 
-    df = df.sample(frac=1).reset_index(drop=True)
-    train_data = df.as_matrix(headers[0:7])
-    test_data = df.as_matrix(headers[7:8])
+    # return algorithm accuracy
+    return 0.0 if count == 0 else count / len(targets_predicted)
 
-    # handle the missing data.
-    df.replace(0, np.NaN ,inplace=True)
-    df.dropna(inplace=True)
+# prints the accuracy of a given data set
+def print_accuracy(classifier_name, data_set_name, accuracy):
+    print("CALLED print_accuracy \n")
+    print("\nThe " + classifier_name + " was " + str(round(accuracy * 100, 3)) +
+          "% accurate on the " + data_set_name + " Data Set.")
 
-    return train_data, test_data
+def test_algorithm():
+    print("CALLED test_algorithm \n")
 
+    # get the data set,classifier, number of tests
+    data_set, data_set_name, is_regressor_data = get_data_set()
+    classifier, classifier_name = get_classifier(is_regressor_data)
+    k = get_num_tests()
+
+    # gets the accuracy of the the algorithm on the data set
+    accuracy = k_cross_validation(data_set, classifier, k, classifier_name)
+
+    # display accuracy
+    print_accuracy(classifier_name, data_set_name, accuracy)
+
+# compares multiple algorithms for user convenience 
+def compare_algorithms():
+    print("CALLED Compare_algorithms \n")
+
+    # get the data set
+    data_set, data_set_name, is_regressor_data = get_data_set()
+
+    # get the classifiers the user wants to compare
+    classifiers = get_multiple_classifiers(is_regressor_data)
+
+    # get number of times the user wants to run the classifier on the data
+    k = get_num_tests()
+
+    # displays all of the classifier's accuracy
+    for classifier_name in classifiers.keys():
+        accuracy = k_cross_validation(data_set, classifiers[classifier_name], k,
+                                      classifier_name != "Decision Tree Classifier")
+        print_accuracy(classifier_name, data_set_name, accuracy)
+
+def main():
+    print("Called Main \n")
+    print("What would you like to do?")
+    print("1 - Test an algorithm")
+    print("2 - Compare multiple algorithms")
+    
+    user_input = int(input("> "))
+
+    if user_input == 1:
+        test_algorithm()
+    elif user_input == 2:
+        compare_algorithms()
+    else:
+        print("ERROR! invalid input")
 
 if __name__ == "__main__":
     main()
